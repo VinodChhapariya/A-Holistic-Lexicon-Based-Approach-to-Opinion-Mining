00593ac288cb3c729df8a317ddfe96a3321d67fe
VinodChhapariya <vinodchhapariya10@gmail.com>
2016-11-23 22:38:09 +0530

    Add files via upload

--- AlgorithmOrientation.py
+++ AlgorithmOrientation.py
@@ -5,16 +5,19 @@ from textblob import TextBlob
 import nltk
 import glob
 import json
+from nltk.corpus import wordnet as wn
 
 ProductFeatureList=[]
+PossibleFeatureList=[]
 
 #features list for given product 
-with open('C:\Users\Vinod Chhapariya\Desktop\TDBMS\Project\Features_Json_Files\Nokia6610.json') as data_file:    
+with open('C:\Users\Vinod Chhapariya\Desktop\TDBMS\Project\Features_Json_Files\HitachiRouter.json') as data_file:    
         data = json.load(data_file)
 for key in data:
         ProductFeatureList.append(str(key))
 print ProductFeatureList
 print len(ProductFeatureList)
+
 
 
 #lists to store positive and negative opinion words
@@ -27,7 +30,7 @@ for filename in OpinionWordFile1:
         for line in open(filename, 'r').readlines():
             t=word_tokenize(line)
             for tags in t:
-                OpinionWordsPositive.append(tags)
+                OpinionWordsPositive.append(tags.upper())
 
 #getting negative opinion words to list
 OpinionWordFile2 = glob.glob('C:/Users/Vinod Chhapariya/Desktop/TDBMS/Project/Opinion Words/negative-words.txt')
@@ -35,19 +38,22 @@ for filename in OpinionWordFile2:
         for line in open(filename, 'r').readlines():
             t=word_tokenize(line)
             for tags in t:
-                    OpinionWordsNegative.append(tags)
+                    OpinionWordsNegative.append(tags.upper())
+
 
 #getting reviews for product
-fileList =glob.glob('C:/Users/Vinod Chhapariya/Desktop/TDBMS/Project/Nokia6610_Reviews/*')
-FeatureDictionary={}
+fileList =glob.glob('C:/Users/Vinod Chhapariya/Desktop/TDBMS/Project/HitachiRouter_Reviews/*')
+
 #feature dictionary to store feature with its orientation score
+FeatureDictionary={}
+FeatureAdjectiveDictionary={}
 for p in ProductFeatureList:
         FeatureDictionary[p]=0
 
-
+#rule for sentences having but clause  
 def ButClauseRule(Words,feature):
         for ow in Words.keys():
-                if Words[ow]> Words['but']:
+                if Words[ow]> Words['BUT']:
                         if ow in OpinionWordsPositive:
                                 Orientation=-1
                         elif ow in OpinionWordsNegative:
@@ -58,6 +64,8 @@ def ButClauseRule(Words,feature):
                                 
 
 
+AdjDictionary={}
+
 #processing each review
 for filename in fileList:
     print filename
@@ -67,18 +75,21 @@ for filename in fileList:
         tokens = word_tokenize(line)
         i = 0
         for t in tokens:
-            Words[t] = i
+            Words[t.upper()] = i
             i += 1
-        #print Words
         
         #noun phrases and nouns are possible features storing them in PosiibleFeatures list
         tb = TextBlob(line)
-        PossibleFeatures = tb.noun_phrases
+        Features = tb.noun_phrases
+        PossibleFeatures=[]
+        for f in Features:
+                f=f.upper()
+                PossibleFeatures.append(f)
         postags = tb.tags
         for t in postags:
             if t[1][0] == 'N':
-                PossibleFeatures.append(t[0])
-        #print PossibleFeatures
+                PossibleFeatures.append(t[0].upper())
+        
 
 
 
@@ -97,19 +108,21 @@ for filename in fileList:
                 #feaure may have more than one word(eg. 'picture quality'), hence considering only first word to calculate distance of the feature from opinion word.
                 token=word_tokenize(feature)
 
+                #Applying But Clause Rule
+                if 'BUT' in Words.keys():
+                        if Words['BUT']<Words[token[0]]:  
+                                Orientation= ButClauseRule(Words,token[0])
+                        else:
+                                del Words['BUT']
+                
                 #checking for opinion word
                 for ow in Words:
                     if ow not in token:    
-                            #Applying But Clause Rule
-                            if 'but' in Words.keys() and Words['but']<Words[token[0]]:  
-                                Orientation= ButClauseRule(Words,token[0])
-
+                            
                             #getting orientation for positive opinion word      
-                            elif ow in OpinionWordsPositive:
-                                #print ow
-
+                            if ow in OpinionWordsPositive:
                                 #handling negation rule
-                                if 'not' in Words.keys() and Words[ow]==Words['not']+1:
+                                if 'NOT' in Words.keys() and Words[ow]==Words['NOT']+1 or 'TOO' in Words.keys() and Words[ow]==Words['TOO']+1:
                                         Orientation=-1
                                 else:
                                         Orientation=1
@@ -118,33 +131,82 @@ for filename in fileList:
                             elif ow in OpinionWordsNegative:
                                 #print ow
                         
-                                if 'not' in Words.keys() and Words[ow]==Words['not']+1:
+                                if 'NOT' in Words.keys() and Words[ow]==Words['NOT']+1 or 'TOO' in Words.keys() and Words[ow]==Words['TOO']+1:
                                         Orientation=1
                                 else:
                                         Orientation =-1
 
                             #calculating distance between feature and each ow
-                            Distance= Words[ow] - Words[token[0]]
+                            x=len(token)
+                            Distance= Words[ow] - Words[(token[x-1])]
                         
                             #orientation formula based on distance between feature and ow
                             Orientation = (Orientation/(abs(Distance)*1.00))
                             FeatureDictionary[feature] += Orientation
+
+                
+               
+
+                
+                tb = TextBlob(feature)
+                posfeature = tb.tags
+                for t in posfeature:
+                    if t[1][0] == 'J':
+                            if feature in AdjDictionary.keys():
+                                AdjDictionary[feature] += Orientation
+                            else:
+                                 AdjDictionary[feature] = Orientation   
+                    else:
+                            for ow in Words.keys():
+                                    tb=TextBlob(ow)
+                                    postag=tb.tags
+                                    for t in postag:
+                                            if t[1][0]=='J':
+                                                    FeatureAdjective=[feature,t[0]]
+                                                    if (feature,t[0]) in  FeatureAdjectiveDictionary.keys():
+                                                            FeatureAdjectiveDictionary[(feature,t[0])] += [Orientation,Orientation]
+                                                    else:
+                                                            FeatureAdjectiveDictionary[(feature,t[0])]=[0,0]
+
+                # Context dependent opinion words handling
+                    if FeatureDictionary[feature]==0:
+                            tb = TextBlob(feature)
+                            posfeature = tb.tags
+                            for t in posfeature:
+                                    if t[1][0] == 'J':
+                                            FeatureDictionary[feature] = AdjDictionary[feature]
+                                    else:
+                                            for i in wn.synsets(ow):
+                                                    for j in i.lemmas(): # Iterating through lemmas for each synset.
+                                                            if j.name() in OpinionWordsPositive:
+                                                                    FeatureDictionary[feature]=1
+                                                            elif j.name() in OpinionWordsNegative:
+                                                                    FeatureDictionary[feature]=-1
+                                                                    for k in j.antonyms():
+                                                                            if k.name() in OpinionWordsPositive:
+                                                                                    FeatureDictionary[feature]=-1
+
+                                                                            elif k.name() in OpinionNWrdsNegative:
+                                                                                    FeatureDictionary[feature]=1
+
+
+
+
+print FeatureDictionary
+
+for feature in  FeatureDictionary.keys():
+        if FeatureDictionary[feature]>=0:
+                FeatureDictionary[feature]=1
+        elif FeatureDictionary[feature]<0:
+                FeatureDictionary[feature]=-1
+       
                         
         #printing features with their score. score>0 -->positive orientation and score<0 --->Negative orientation
 print FeatureDictionary
 print len(FeatureDictionary)
 
-for feature in FeatureDictionary:
-        if FeatureDictionary[feature]>=0:
-                FeatureDictionary[feature]=1
-        elif FeatureDictionary[feature]<0:
-                FeatureDictionary[feature]=-1
-        #else:
-                #FeatureDictionary.pop(feature)
 
-print FeatureDictionary
-
-with open('Nokia6610_Predicted.json', 'w') as outfile:
+with open('HitachiRouter_Predicted.json', 'w') as outfile:
      json.dump(FeatureDictionary, outfile, sort_keys = True, indent = 4,ensure_ascii=False)
 
 
@@ -154,7 +216,6 @@ with open('Nokia6610_Predicted.json', 'w') as outfile:
                 #applying synonym antonym rule
                 #But caluse rule - done
                 #calculating accuracy of the algorithm - done
-                
 
 
                         
